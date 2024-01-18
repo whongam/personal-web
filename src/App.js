9f0306d9fef025c260a5fe1a8a9d2f4a8772b6b4
whongam <k1522151520@gmail.com>
2024-01-19 01:20:28 +0800

    Add fonts and update routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Home from "./scenes/home";
-import Contacts from "./scenes/contacts";
-import About from "./scenes/about";
-import Projects from "./scenes/projects";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
+import HomePage from "./pages/HomePage";
+// import ActivityPage from "./pages/ActivityPage";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
-
-function App() {
-  const [theme, colorMode] = useMode();
-
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <main className="content">
-            <Topbar/>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contacts" element={<Contacts />} />
-
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
+export default function App() {
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
-
-export default App;
